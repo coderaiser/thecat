36734262918f5bc69ee856a36733b0c18c5961dc
coderaiser <coderaiser@cloudcmd.io>
2017-01-18 12:41:56 +0200

    chore(thecat) dos2unix

--- bin/thecat.js
+++ bin/thecat.js
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
-    
-    var http        = require('http'),
-        express     = require('express'),
-        thacat      = require('..'),
-        PORT        = 1234,
-        app         = express();
-    
-    http.createServer(app).listen(PORT);
-    
-    console.log('server:' + PORT);
-    
-    app.get('/', function(req, res) {
-        res.redirect('/cat.png');
-    });
-    
-    app.use(thacat({
-        prefix: '/cat.png'
-    }));
-})();
+'use strict';
+
+var http        = require('http'),
+    express     = require('express'),
+    thacat      = require('..'),
+    PORT        = 1234,
+    app         = express();
+
+http.createServer(app).listen(PORT);
+
+console.log('server:' + PORT);
+
+app.get('/', function(req, res) {
+    res.redirect('/cat.png');
+});
+
+app.use(thacat({
+    prefix: '/cat.png'
+}));
+
